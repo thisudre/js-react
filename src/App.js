223085a7c4e15f82224148c36c3ffb7292820cee
Thiago <thiagoty@gmail.com>
2019-12-16 11:46:59 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './css/pure-min.css';
+import './css/side-menu.css';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado.js';
+import BotaoCustomizado from './componentes/BotaoCustomizado.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {lista: [], nome:'', email:'', senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: 'https://cdc-react.herokuapp.com/api/autores',
+            dataType: 'json',
+            success: function(resposta) {
+                this.setState({lista:resposta});
+            }.bind(this)
+        })
+    }
+
+    enviaForm(evento){
+        evento.preventDefault();
+        $.ajax({
+            url: 'https://cdc-react.herokuapp.com/api/autores',
+            contentType: 'application/json',
+            dataType: 'json',
+            type: 'post',
+            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+            success: function(resposta) {
+                this.setState({lista: resposta});
+            }.bind(this),
+            error: function(resposta) {
+                console.log(resposta);
+            }
+        })
+    }
+
+    setNome(evento){
+        this.setState({nome:evento.target.value});
+    }
+
+    setEmail(evento){
+        this.setState({email:evento.target.value});
+    }
+
+    setSenha(evento){
+        this.setState({senha:evento.target.value});
+    }
+
+    render() {
+        return (
+            <div id="layout">
+
+                <a href="#menu" id="menuLink" className="menu-link">
+                    <span></span>
+                </a>
+
+                <div id="menu">
+                    <div className="pure-menu">
+                        <a className="pure-menu-heading" href="#">Company</a>
+                        <ul className="pure-menu-list">
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div id="main">
+                    <div className="header">
+                        <h1>Cadastro de Autores</h1>
+                    </div>
+                    <div className="content" id="content">
+                        <div className="pure-form pure-form-aligned">
+                            <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method='post'>
+                                <InputCustomizado id='name' type='text' name='name' value={this.state.nome} label='Nome' onChange={this.setNome}/>
+                                <InputCustomizado id='email' type='email' name='email' value={this.state.email} label='Email' onChange={this.setEmail}/>
+                                <InputCustomizado id='senha' type='password' name='senha' value={this.state.senha} label='Senha' onChange={this.setSenha}/>
+                                <BotaoCustomizado type='submit' label='Gravar'/>
+                            </form>
+
+                        </div>
+                        <div>
+                            <table className="pure-table">
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.lista.map(function(autor) {
+                                        return(
+                                            <tr key={autor.id}>
+                                                <td>{autor.nome}</td>
+                                                <td>{autor.email}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
