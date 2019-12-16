df1b4e37cd9102a4c98d818110e7533ef5b8f49d
Thiago <thiagoty@gmail.com>
2019-12-16 15:08:30 -0300

    refatoracao

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado.js';
-import BotaoCustomizado from './componentes/BotaoCustomizado.js';
+import {FormularioAutor} from './componentes/Autor.js'
 
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {lista: [], nome:'', email:'', senha:''};
-        this.enviaForm = this.enviaForm.bind(this);
-        this.setNome = this.setNome.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setSenha = this.setSenha.bind(this);
+        this.state = {lista: []};
     }
 
     componentDidMount() {
@@ -24,35 +19,6 @@ class App extends Component {
                 this.setState({lista:resposta});
             }.bind(this)
         })
-    }
-
-    enviaForm(evento){
-        evento.preventDefault();
-        $.ajax({
-            url: 'https://cdc-react.herokuapp.com/api/autores',
-            contentType: 'application/json',
-            dataType: 'json',
-            type: 'post',
-            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
-            success: function(resposta) {
-                this.setState({lista: resposta});
-            }.bind(this),
-            error: function(resposta) {
-                console.log(resposta);
-            }
-        })
-    }
-
-    setNome(evento){
-        this.setState({nome:evento.target.value});
-    }
-
-    setEmail(evento){
-        this.setState({email:evento.target.value});
-    }
-
-    setSenha(evento){
-        this.setState({senha:evento.target.value});
     }
 
     render() {
@@ -67,9 +33,9 @@ class App extends Component {
                     <div className="pure-menu">
                         <a className="pure-menu-heading" href="#">Company</a>
                         <ul className="pure-menu-list">
-                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+                            <li className="pure-menu-item"><a href="." className="pure-menu-link">Home</a></li>
+                            <li className="pure-menu-item"><a href="." className="pure-menu-link">Autor</a></li>
+                            <li className="pure-menu-item"><a href="." className="pure-menu-link">Livro</a></li>
                         </ul>
                     </div>
                 </div>
@@ -79,21 +45,13 @@ class App extends Component {
                         <h1>Cadastro de Autores</h1>
                     </div>
                     <div className="content" id="content">
-                        <div className="pure-form pure-form-aligned">
-                            <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method='post'>
-                                <InputCustomizado id='name' type='text' name='name' value={this.state.nome} label='Nome' onChange={this.setNome}/>
-                                <InputCustomizado id='email' type='email' name='email' value={this.state.email} label='Email' onChange={this.setEmail}/>
-                                <InputCustomizado id='senha' type='password' name='senha' value={this.state.senha} label='Senha' onChange={this.setSenha}/>
-                                <BotaoCustomizado type='submit' label='Gravar'/>
-                            </form>
-
-                        </div>
+                        <FormularioAutor/>
                         <div>
                             <table className="pure-table">
                                 <thead>
                                     <tr>
                                         <th>Nome</th>
-                                        <th>email</th>
+                                        <th>Email</th>
                                     </tr>
                                 </thead>
                                 <tbody>
---
+++ src/componentes/Autor.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import InputCustomizado from './InputCustomizado';
+import BotaoCustomizado from './BotaoCustomizado';
+import $ from 'jquery';
+
+export class FormularioAutor extends Component {
+    constructor(){
+        super();
+        this.state = {lista: [], nome:'', email:'', senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    enviaForm(evento){
+        evento.preventDefault();
+        $.ajax({
+            url: 'https://cdc-react.herokuapp.com/api/autores',
+            contentType: 'application/json',
+            dataType: 'json',
+            type: 'post',
+            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+            success: function(resposta) {
+                this.setState({lista: resposta});
+            }.bind(this),
+            error: function(resposta) {
+                console.log(resposta);
+            }
+        })
+    }
+
+    setNome(evento){
+        this.setState({nome:evento.target.value});
+    }
+
+    setEmail(evento){
+        this.setState({email:evento.target.value});
+    }
+
+    setSenha(evento){
+        this.setState({senha:evento.target.value});
+    }
+
+
+    render(){
+        return (
+            <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method='post'>
+                    <InputCustomizado id='name' type='text' name='name' value={this.state.nome} label='Nome' onChange={this.setNome}/>
+                    <InputCustomizado id='email' type='email' name='email' value={this.state.email} label='Email' onChange={this.setEmail}/>
+                    <InputCustomizado id='senha' type='password' name='senha' value={this.state.senha} label='Senha' onChange={this.setSenha}/>
+                    <BotaoCustomizado type='submit' label='Gravar'/>
+                </form>
+            </div>
+        );
+    }
+}
